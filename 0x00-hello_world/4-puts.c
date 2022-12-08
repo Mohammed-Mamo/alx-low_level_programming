be3828d27e359f220196976859a8a0c7e9fa7678
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-08 15:58:33 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 /**
- * main function is about to start
- */
- int main (void)
- {
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 
- }
+}
