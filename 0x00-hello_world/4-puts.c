cd5f7df5abc5464177eaae5d593c16fb01053548
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-09 04:13:45 -0800

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 /**
- *
- * *main -Entry point
- *
- * *somecodes
- *
- * *Return:0 (Succes)
- *
- * */
+ *main -Entry point
+ *somecodes
+ * Return:0 (Succes)
+ */
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
-
-
 	return (0);
-
-
-
 }
