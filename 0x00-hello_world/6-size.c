417e6f2919b204d15a667c6600b5247ff18fee5f
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-09 05:22:46 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 /**
- * Main function below
+ * main
+ * somecodes
+ * Return :0 (Succes)
  */
-int main (void)
+
+int main(void)
 {
-	char x;
-	int y;
-	long int z;
-	long long int w;
-	float T;
+
+char x; int y;long int z;long long int w;float T;
+/**
+ * printinting size of different data types
+ */
 printf("size of a char: %lubyte(s)\n",(unsigned long)sizeof(x));
 printf("Size of an int: %lubyte(s)\n",(unsigned long)sizeof(y));
 printf("Size of a long int: %lubyte(s)\n",(unsigned long)sizeof(z));
 printf("Size of a long long int:%lubyte(s)\n",(unsigned long)sizeof(w));
 printf("Size of a float: %lubyte(s)\n",(unsigned long)sizeof(T));
+
 return (0);
+
 }
