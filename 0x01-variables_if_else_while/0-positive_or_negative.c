71cbe9d060ad45ede2e635192ab8413442c9a645
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-09 18:09:51 -0800

    TASK 0 FOR 100 chance

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /**
 * main - Entry point
@@ -8,15 +8,15 @@
 */
 int main(void)
 {
-
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 else if (n == 0)
-printf("%d is zero\n", n);
-else 
-printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
+else
+	printf("%d is negative\n", n);
 return (0);
 }
