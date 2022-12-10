b0624b252f085ef3240c5c1345f7a53f541cb4c3
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-09 18:59:39 -0800

    ADvanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/** 
-* main - Entry point * * 
+/**
+* main - Entry point
 *
-* Return: Always 0 (Success)*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int k, j;
-	for k = '0'; k < '9'; k++)
-	{
+
+for (k = '0'; k < '9'; k++)
+{
 	for (j = k + 1; j <= '9'; j++)
 	{
 		if (j != k)
 		{
-	putchar(k);
-	putchar(j);
+			putchar(k);
+			putchar(j);
 			if (k == '8' && j == '9')
 				continue;
-putchar(',');
-putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	  }
+}
 putchar('\n');
-
 return (0);
 }
