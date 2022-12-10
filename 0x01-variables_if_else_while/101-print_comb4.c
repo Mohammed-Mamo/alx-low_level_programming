198d8bba43d95981b7eda2f1b11d24e08b152973
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-09 19:10:18 -0800

    advanced 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,32 +4,32 @@
 /**
 *
 * main -starts
-* body  
+* body
 *return 0: (succes)
 */
 int main(void)
-
 {
 	int d, p, q;
+
 for (d = '0'; d < '9'; d++)
 {
-for (p = d + 1; p <= '9'; p++)
+	for (p = d + 1; p <= '9'; p++)
 	{
-for (q = p + 1; q <= '9'; q++)
-{
-if ((p != d) != q)
-{
-putchar(d);
-putchar(p);
-putchar(q);
-if (d == '7' && p == '8')
-	continue;
-putchar(',');
-putchar(' ');
+		for (q = p + 1; q <= '9'; q++)
+		{
+			if ((p != d) != q)
+			{
+				putchar(d);
+				putchar(p);
+				putchar(q);
+				if (d == '7' && p == '8')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 }
-}
-}
-putchar('\n');	
+putchar('\n');
 return (0);
 }
