5b0245a8ee827369be0fa1545321dd8b180a7cea
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-12 10:30:23 -0800

    fputs() function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
+*main - C code that outputs using fputs
+*Return: Always 1 (Success)
+*/
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	
+	return (1);
 }
