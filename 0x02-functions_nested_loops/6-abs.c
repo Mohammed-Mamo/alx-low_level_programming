7b463c6cc90b2594b12fd6dcdabc225f9ac74ae2
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-13 14:26:48 -0800

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,19 @@
 #include "main.h"
+
 /**
- * _abs chech main
- * r: An integre input
- *Description: This function returns absolute value of a number
- *Return: Absolut value of number r
- */
+* _abs - chech main
+* @r: An integre input
+* Description: This function returns absolute value of a number
+* input r of the function is considered to be an integer
+* the out put is also an positive integer
+* Return: Absolut value of number r
+*/
 
 int _abs(int r)
+
 {
-if (r >= 0)
-return (r);
-return (r * -1);
+	if (r >= 0)
+		return (r);
+	else
+	       return (r * -1);
 }
