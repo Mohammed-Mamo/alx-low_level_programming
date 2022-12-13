ed0faa157e3f4cc5e410cd14d2dc405d413c04f6
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-13 11:05:24 -0800

    3rd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* _islower is function that checks lower case
+* @c: The character to be checked
+ * _islower is function that checks lower case
  * Return 1 is true
  */
 int _islower(int c)
