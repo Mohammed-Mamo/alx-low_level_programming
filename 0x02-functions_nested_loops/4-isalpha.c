6014852490faf5a388c330d2ed26c89bd3d0649f
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-13 14:49:11 -0800

    fourth

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,15 @@
 
 /**
 * _isalpha - checks alphabet or others
-*c -is a input character
+* it wil differentiatwe between alphapbetas upper or lower and other symbols
+* it uses one variable
+* @c:-is a input character
 * Return : 1 for  alphabet ,0 for others
 */
 
 int _isalpha(int c)
 {
-	if( (c <= 122 && c >= 97) || (c >=65 && c <= 90) )
+	if ((c <= 122 && c >= 97) || (c >= 65 && c <= 90) )
 		return (1);
 	else
 		return (0);
