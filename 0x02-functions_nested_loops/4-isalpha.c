b67d45f17a2e1d395a577de47ff6be7fa1807d93
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-13 11:39:22 -0800

    4th

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * description  the function checks whether argument is a letter or digit
- * 4-isalpha begin by taking argument
+ * description: the function checks whether argument is a letter or digit
+ * _isalpha begin by taking argument c as an integer
  * returns 1 if letter , 0 otherwise
  */
 int _isalpha(int c)
 {
-	if((c <= 122 && c>=97) || (c >=65 && c <= 90))
+	if((c <= 122 && c >= 97) || (c >=65 && c <= 90))
 		return (1);
 	else
 		return (0);
