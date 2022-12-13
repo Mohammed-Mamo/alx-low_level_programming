fbc0ebee12b18b9e1fb0acb87ae55a83663fd15f
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-13 13:34:53 -0800

    10thh

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-* add - Check Holberton
+* add - Checkmain
 * @a: An integer a
 * @b: An integer b
 * Description: Function that adds two integers and returns the result
 * Return: Result to add a and b
- */
- 
- int add(int a, int b)
+*/
+int add(int a, int b)
 {
-	return (a + b);
+return (a + b);
  }
