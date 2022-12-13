2db57182fcdd9dbe15f7cbe89122de1821eb4261
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-13 14:42:27 -0800

    5th

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,32 @@
 #include "main.h"
+
 /**
- * this function displays number with its sign
- * print_sign(int n) takes  an integer
- * n is an input to the function
- * return : 1, for positive ,0 for zero -1 for negative
+* this function displays number with its sign
+* print_sign - check main
+* @n: is an input to the function
+* Description: it will display magnitude and sign also
+* specifically used for negative, positive or zero
+* Return : 1, for positive ,0 for zero -1 for negative
  */
 int print_sign(int n)
 {
-	char neg, pos ;
+	char neg, pos;
+
 	neg = '-';
 	pos = '+';
-
-	if (n>0)
-	{
-		_putchar(pos);
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar(0);
-		return (0);
-	}
-	else 
-	{
-		_putchar(neg);
-		return (-1);
-	}
+if (n > 0)
+{
+	_putchar(pos);
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar(0);
+	return (0);
+}
+else
+{
+	_putchar(neg);
+	return (-1);
+}
 }
