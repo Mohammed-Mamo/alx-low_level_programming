b4d29dcbd87d33f4827971f22eda3ed309e60aee
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 04:52:06 -0800

    SIGN-MAGNITUDE

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
-* this function displays number with its sign
-* print_sign - check main
-* @n: is an input to the function
-* Description: it will display magnitude and sign also
-* specifically used for negative, positive or zero
-* Return : 1, for positive ,0 for zero -1 for negative
+ * This function prints sign of a number
+ * print_sign - check main
+ * @n: is an input to the function
+ * Return: values 1 if +ve, -1 if -ve or 0
  */
 int print_sign(int n)
+
 {
 	int sign = 0;
-
+	
 	if (n > 0)
 	{
-	_putchar('+');
-	sign = 1;
+		_putchar('+');
+		sign = 1;
 	}
 	else if (n == 0)
 	{
-	_putchar('0');
-	sign = 0;
+		_putchar('0');
+		sign = 0;
 	}
 	else if (n < 0)
 	{
-_putchar('-');
-sign = -1;
+		_putchar('-');
+		sign = -1;
 	}
-return (sign);
+	return (sign);
 }
