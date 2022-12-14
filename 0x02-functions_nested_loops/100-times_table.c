eb2645be31dd68f1e99e7377e456d53a1b293815
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 07:04:00 -0800

    Advanced tasks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_times_table - check main
+ * @n: is a positive integer less than 15
+ * Return : value 0
+ * nxn multiplication table
+ */
+void print_times_table(int n)
+{
+	int k;
+
+	if ( n < 15 && n > 0)
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			for (int j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j == n)
+
+								continue; 
+
+							if ((k / 10 == 0)&& j != 0){
+
+								_putchar(' ');
+
+								_putchar(k + '0');}
+
+							else{     
+
+								_putchar((k / 10) + '0');
+
+								_putchar((k % 10) + '0');
+
+								_putchar(',');    
+
+								_putchar(' ');}
+
+						}  
+
+				_putchar('\n');
+
+		}
+
+	}
