431682e160d4e250e80afbdc795cb4e7d3c4fa0a
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 07:53:33 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/** 
+ * putchar display a character _putchar
+ * no return value
+ * no argument
+ */
+int main(void)
+{
+	int t = 0;
+	char word [] = "_putchar";
+	for (t = 0; t < 8; t++)
+		_putchar(word[t]);
+	_putchar('\n');
+
+return (0);
+}
