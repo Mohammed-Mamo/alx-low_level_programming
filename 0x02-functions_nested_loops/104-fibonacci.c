d5402e678f192c8caebdc50f588f70ee037460db
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 13:30:13 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check main
+ * it displays all fibonacci series
+ * return : 0
+ *
+ */
+int main(void)
+{
+	printf("1 , 2\n");
+
+	int j,k;
+	for (j = 3; j < 98; j++)
+	{ sum =2 +j;
+		printf("%d,", sum);
+
