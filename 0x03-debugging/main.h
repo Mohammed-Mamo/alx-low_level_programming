6d37cdf0880c8e3f6296fb7debd6abc865d36720
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 09:29:21 -0800

    main.h and main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
-int main(void)
-{
-	void positive_or_negative(int i)
-	{
-		if (i < 0)
-			printf("%d is negative\n", i);
-		else if (i > 0)
-			printf("%d is positive\n", i);
-		else 
-			printf("it is zero\n");
-	}
-	return (0);
-}
+#endif /* MAIN_H */
 
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+
+void positive_or_negative(int i);
+int main(void)
+{
+	void positive_or_negative(int i)
+	{
+		if (i < 0)
+			printf("%d is negative\n", i);
+		else if (i > 0)
+			printf("%d is positive\n", i);
+		else
+			printf("it is zero ,neither positive nor negative\n");
+	}
+	return (0);
+}
