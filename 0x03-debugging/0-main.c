8f6a65709887931b5c454806611497b2c02365b2
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 09:08:05 -0800

    main.h and main.c file created

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <sdtio.h>
+#include "main.h"
+/**
+ * main - check main
+ * no argument
+ * call another function called 
+ * return
+ * 
+ */
+int main(void)
+{
+	int i;
+	
+	printf("please enter an integer\n");
+	scanf("%d:", i);
+	positive_or_negative(i);
+	return (0);
+}
+
