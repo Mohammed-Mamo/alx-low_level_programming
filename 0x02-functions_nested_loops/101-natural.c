2e9b3edc0885a1d3e38e187bc00260e826d658ad
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 13:58:09 -0800

    last

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- *
- * main - Entry point
- *
+* main - Entry point
+* it adds all factor of 3 or 5 below 1024
  *Return: Always 0 (Success)
- *
  */
 int main(void)
 {
 	int i, sum = 0;
-	
+
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 5) == 0 || (i % 3) == 0)
