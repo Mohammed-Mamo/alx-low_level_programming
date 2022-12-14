c3e1dbda740d9f33e5b94758e85e15f429478be2
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-14 13:01:49 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-/** 
- * putchar display a character _putchar
- * no return value
- * no argument
- */
+/**
+ *
+ * main - check 
+* return - 0
+*/
 int main(void)
 {
-	int t = 0;
-	char word [] = "_putchar";
-	for (t = 0; t < 8; t++)
-		_putchar(word[t]);
-	_putchar('\n');
 
-return (0);
+int t;
+char n[8] = {'_' , 'p' , 'u','t','c','h','a','r'};
+
+	for (t = 0; t < 8; t++)
+		_putchar(n[t]);
+	_putchar('\n');
+	return (0);
 }
