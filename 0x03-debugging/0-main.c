a9d83dfd2a6b3a21e343f87597057268f05634a4
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-15 12:27:34 -0800

    0main.c modified

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- * main - check main
- * no argument
- * call another function called
- * return: 0 is returned 
- */
+* main - check main
+* no argument
+* call another function called
+* Return: 0 is returned 
+*/
 
 int main(void)
 {
 	int i;
 
-	i=0;
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
