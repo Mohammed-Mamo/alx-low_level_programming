f548a15d09651fafc921452afc36462fef5aac64
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-15 12:58:58 -0800

    i casung infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,8 @@
 
 int main(void)
 {
-	
 	int i;
-
+	
 	printf("Infinite loop incoming :(\n");
 	
 	i = 0;
@@ -22,4 +21,5 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
+
 }
