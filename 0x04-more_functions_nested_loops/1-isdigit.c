592af7403b0bada35836c0a07c05d3523a7ac091
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-16 11:41:59 -0800

    code correction

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,15 +5,11 @@
  * @c: is an input to be tested
  * Return: 1 if c is a digit returns 0 otherwise
  */
-
-
 int _isdigit(int c)
 {
 	int digit =0;
-
-	if (c <= 9 && c >=0)
-		digit = 1;
 	
+	if (c <= 9 && c >= 0)
+		digit = 1;
 	return (digit);
 }
-
