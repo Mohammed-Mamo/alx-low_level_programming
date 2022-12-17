ea4778940e99135860980090a46e1362f485d3e4
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 12:33:04 -0800

    fgfg

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* print_number - prints digits 0 -9
-*
-* Return: void
-*/
+ * print_number - prints digits 0 -9
+ *
+ * Return: void
+ */
 
 void print_numbers(void)
 {
-	int i = '0';
-	
-	for (; i <= '9'; i++)
+	int i ='0';
+
+	for (;i <= '9';i++)
 		_putchar(i);
 	_putchar('\n');
 }
