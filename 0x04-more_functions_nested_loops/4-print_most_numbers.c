2a01ff0f16b171940df66d6339ac335b05c60485
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 15:16:59 -0800

    Coding is caring

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,11 @@ void print_most_numbers(void)
 {
 	int i = '0';
 
-	while (i < '10')
+	while (i <= '9')
 	{
 		if (i != '2' && i != '4')
 			_putchar(i);
 		i++;
 	}
-	_putchar('\n);
+	_putchar('\n');
 }
-
-
