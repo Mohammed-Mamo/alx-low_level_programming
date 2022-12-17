7925f2c2ed421d6f1361e953df29d0520d77ac43
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 15:22:43 -0800

    commit reqires commitment

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,12 @@
 
 void print_square(int size)
 {
-	int r;
-	for (int i = 0; i < size ;i++) 
+	int r = n , i = 0;
+
+	for (; i < size ;i++) 
 	{
-		for (r = n ; r > 1;r--)
-		{
+		for (; r > 1; r--)
 		_putchar('#');
-		}
 		_putchar('\n');
 	}
 }
