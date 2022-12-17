23d8e8ea27a7ed305fab54e88c599bc406e374fc
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 14:46:30 -0800

    7-print

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * print diagonal
- *
- * return:
+ * print_diagonal -prints symbol
+ * #n: is height
+ * Return: nothing
  */
 
 void print_diagonal(int n)
 {
 	int i = 0, j;
 
-	if (n > 0)
+	if(n > 0)
 	{
-		for (; i < n; i++)
+		for(; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
-				_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
-	else
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar(92);
 		_putchar('\n');
+	}
+}
+else
+_putchar('\n');
 }
