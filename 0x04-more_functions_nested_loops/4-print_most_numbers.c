c3a0505f642199e5c189ffaf4aa0a8c9e8b5b036
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 13:05:44 -0800

    correct

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 {
 	int j = '0';
 
-	for (; j < 10; j++)
+	for (; j < '10'; j++)
 	{
-		if (j != '2' && j != 4)
+		if (j != '2' || j != '4')
 		_putchar(j);
 	}
 	_putchar('\n');
-}
+i}
