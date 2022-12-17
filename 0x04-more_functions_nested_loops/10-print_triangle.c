3fc6ffba5673a1ba0428c9de231d0903c307ba6d
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 15:53:55 -0800

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,7 +6,7 @@
  * Return: no
  */
 
- void print_triangle(int size)
+void print_triangle(int size)
 {
 	int i = 0, j, n = size - 1;
 
@@ -15,7 +15,7 @@
 	while (i < size)
 	{
 	i++;
-	for (j = 0;j < size; j++)
+	for (j = 0 ; j < size ; j++)
 	{
 	if (j < n)
 	_putchar(' ');
@@ -29,4 +29,3 @@
 	else
 		_putchar('\n');
 }
-	
