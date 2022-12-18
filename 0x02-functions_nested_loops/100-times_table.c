8032994952907b4a44f1f0d03592277658d00b03
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-18 08:38:54 -0800

    n by n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - check main
  * @n: is a positive integer less than 15
@@ -16,30 +17,25 @@ void print_times_table(int n)
 			for (int j = 0; j <= n; j++)
 			{
 				k = i * j;
+
 				if (j == n)
-
-								continue; 
-
-							if ((k / 10 == 0)&& j != 0){
-
-								_putchar(' ');
-
-								_putchar(k + '0');}
-
-							else{     
-
-								_putchar((k / 10) + '0');
-
-								_putchar((k % 10) + '0');
-
-								_putchar(',');    
-
-								_putchar(' ');}
-
-						}  
-
-				_putchar('\n');
-
+					continue;
+				if ((k / 10 == 0) && (j != 0))
+				{
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else
+				{
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
-
 	}
+	else
+		_putchar('\n');
+}
