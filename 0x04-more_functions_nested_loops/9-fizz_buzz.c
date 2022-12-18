c5c8b293d64e9af7d747d08680bdf17723922759
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 16:04:22 -0800

    FIZZBUZZ

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - entry point
+ * no input
+ * Return: 0 succes
+ */
+
+int main(void)
+{
+	int k = 1;
+
+	while (k < 100)
+	{
+		if (k % 3 == 0 && k % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
