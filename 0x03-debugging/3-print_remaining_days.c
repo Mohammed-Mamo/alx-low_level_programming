8a126a0a057c116889cfc9966df9ad352f903638
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-17 17:30:54 -0800

    last commit after 50

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,6 +17,7 @@ void print_remaining_days(int month, int day, int year)
 		if (month >= 2)
 		{
 			day++;
+		}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
@@ -32,5 +33,4 @@ void print_remaining_days(int month, int day, int year)
 		printf("Remaining days: %d\n", 365 - day);
 	}
 	}
-}
 }
