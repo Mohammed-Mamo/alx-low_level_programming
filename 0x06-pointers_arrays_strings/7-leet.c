fe9ce4eaf34ca88eacda3c2f04a9c5d4cb7b2e25
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 14:28:54 -0800

    7thtask

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
- * leet - function encodes to
- * @x: input pointer
- *
- *Return: character
+* leet - function encodes to
+* @x: input pointer
+*Return: character
 */
 
 char *leet(char *x)
 {
 	int i = 0, j = 0;
+
 	char encode[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	char alphabet[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 	while (x[j])
 	{
-		for (; i < 10 ; i++)
+		for (; i < 10; i++)
+		{
 			if (x[j] == alphabet[i])
 				x[j] = encode[i];
+		}
 		j++;
 	}
-return (x);
+	return (x);
 }
