6607987f96f7d1ee7ee7519c80d6800c9ef1d173
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 15:16:13 -0800

    task six

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,15 @@
 char *cap_string(char *ltr)
 {
 	int i = 0 , j;
-	char Separators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
+	char Separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
 
 	while (ltr[i])
 	{
-		if (ltr[i] >= 'a' && ltr[i] <= 'z')
-			ltr[i] -= 32;
-		for (j =0; j < 13; j++)
+		if (i == 0 && (ltr[i] >= 'a' && ltr[i] <= 'z'))
+			ltr[i] = ltr[i] - 32;
+		else
+		{
+			for (j = 0; j < 13; j++)
 			if (ltr[i] == Separators[j] && (ltr[i + 1] >= 'a' && ltr[i + 1] <= 'z'))
 				ltr[i + 1] -= 32;
 		i++;
