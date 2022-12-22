4aa26ef42f349ac56839ad6d697bf8e1df880737
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 15:58:34 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,20 +3,16 @@
  * _strcmp - function
  * @s1: source1
  * @s2: source2
- * return : integer
+ * Return: integer
  *
  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
 	{
-		if (*s1 != *s2)
-		{
+		if ((*s1 != *s2))
 			return (*s1 - *s2);
-			break;
-		}
 		s1++, s2++;
 	}
 	return (*s1 - *s2);
 }
-
