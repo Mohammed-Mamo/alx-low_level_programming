87120e92d122e0838620946a19f07ca6949a2670
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 15:18:04 -0800

    Brace matters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,6 +21,7 @@ char *cap_string(char *ltr)
 			for (j = 0; j < 13; j++)
 			if (ltr[i] == Separators[j] && (ltr[i + 1] >= 'a' && ltr[i + 1] <= 'z'))
 				ltr[i + 1] -= 32;
+		}
 		i++;
 	}
 	return (ltr);
