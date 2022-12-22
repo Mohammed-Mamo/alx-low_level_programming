d5312a839a7babfab2ee65132916205212b98c64
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 14:55:30 -0800

    6th task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,24 @@
 #include "main.h"
+
 /**
- * cap_string - function
- * @*: text
- * Return: capitalized
+* cap_string - function
+* @ltr: upper case text
+*
+* Return: capitalized
 */
+
 char *cap_string(char *ltr)
 {
-	int i =0;
+	int i = 0 , j;
+	char seperatos[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
 
 	while (ltr[i])
 	{
 		if (ltr[i] >= 'a' && ltr[i] <= 'z')
 			ltr[i] -= 32;
+		for (j =0; j < 13; j++)
+			if (ltr[i] == separator[j] && ltr[i] >= 'a' && ltr[i] <= 'z')
+				ltr[i + 1] -= 32;
 		i++;
 	}
 	return (ltr);
