ab33212e1e5359461d4b0ded11f4f10d2de877b2
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 18:10:21 -0800

    code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,14 @@
 char *leet(char *x)
 {
 	int i = 0, j;
+char NUM[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+   	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	char encode[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	char alphabet[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	while (x[i])
+while (x[i])
 	{
 		for (j = 0; j < 10; j++)
-		{
-			if (x[i] == alphabet[j])
-				x[i] = encode[j];
-		}
+			if (x[i] == str[j])
+				x[i] = NUM[j];
 		i++;
 	}
 	return (x);
