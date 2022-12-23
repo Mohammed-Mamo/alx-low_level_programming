e475929f2b5ef3cc5ace7aca03cd683853fa27c1
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-23 11:36:40 -0800

    6th

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@ char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *small);
 int check_separators(char c);
 char *rot13(char *);
 char *_strncpy(char *dest, char *src, int n);
+char *cap_string(char *);
