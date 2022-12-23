88a4fbdae5687153037e0bff67823f99df30aa55
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-23 11:51:12 -0800

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,15 @@ int check_separators(char c);
 char *cap_string(char *s)
 {
 	int i = 0;
-
+	
 	while (s[i])
 	{
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
+		
 		if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
+
 		i++;
 	}
 	return (s);
@@ -29,7 +31,6 @@ char *cap_string(char *s)
  */
 int check_separators(char c)
 {
-
 	int i = 0;
 	char sep[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?','"', '(', ')', '{', '}' };
 	
