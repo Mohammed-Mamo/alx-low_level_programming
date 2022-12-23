8daf14b03dc4cff5579e9a3295fef6180a2dad88
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 18:37:33 -0800

    last attempt at 100 chance

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int separator(char c);
 /**
 * cap_string - function
 * @ltr: upper case text
@@ -9,16 +10,31 @@
 char *cap_string(char *ltr)
 {
 	int i = 0;
-	char Separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	
+
 	while (ltr[i])
 	{
 		if (i == 0 && (ltr[i] >= 'a' && ltr[i] <= 'z'))
 			ltr[i] -= 32;
-		for (j = 0; j < 13; j++)
-			if (ltr[i] == Separators[j] && (ltr[i + 1] >= 'a' && ltr[i + 1] <= 'z'))
+		if (separator[ltr] && (ltr[i + 1] >= 'a' && ltr[i + 1] <= 'z'))
 				ltr[i + 1] -= 32;
 		i++;
 	}
 	return (ltr);
+}
+#include "main.h"
+/**
+ * separator - check main
+ * @c: input
+ * Return: 1 succes ,0 fail
+ *
+ */
+int separator(char c)
+{
+	int j;
+	char Separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (j = 0; j < 13; j++)
+		if (c == Separators[j])
+			return (1);
+	return (0);
 }
