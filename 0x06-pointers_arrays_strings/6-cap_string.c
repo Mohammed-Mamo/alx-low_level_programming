e3a385b4bc81cf2f634d9767d08e292d0ef79294
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-23 11:34:37 -0800

    change to upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,24 @@
 #include "main.h"
+
 int check_separators(char c);
 /**
  * cap_string - function
  * @s: input pointer
  * Return: a characte
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (s[i]){if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-
-		s[i] -= 32;
-
+	while (s[i])
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
 		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-
 			s[i + 1] -= 32;
-
 		i++;
-
 	}
-
 	return (s);
-
 }
 
 /**
@@ -32,22 +28,15 @@ char *cap_string(char *)
  *
  */
 int check_seperators(char c)
-
 {
 
 	int i = 0;
-
 	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?','"', '(', ')', '{', '}' };
-
+	
 	for (; i < 13; i++)
-
 	{
-
 		if (c == seperators[i])
-
 			return (1);
-
 	}
-
 	return (0);
 }
