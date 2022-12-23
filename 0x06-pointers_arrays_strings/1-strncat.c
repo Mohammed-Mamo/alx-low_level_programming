ef83c230ef22e0c4b380982c76d439c2739075d1
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 17:35:41 -0800

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,17 @@ char *_strncat(char *dest, char *src, int n)
 	char *aux = dest, *aux2 = src;
 	int j = 0;
 
-	while (j++ < n)
-		*dest++ = *src++;
+	while (*dest != '\0')
+	{
+		dest++;
+		j++;
+	}
+	dest = aux;
+	if ( j > n)
+	n = j;
+
+	while (*src && n--)
+		dest[j++] = *src++;
 	src = aux2;
 
 	return (aux);
