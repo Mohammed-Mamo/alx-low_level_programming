3298eafbf81f8861432f38e2e0bc600faa90597d
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-22 17:19:37 -0800

    copy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,9 +12,6 @@ char *_strncat(char *dest, char *src, int n)
 	char *aux = dest, *aux2 = src;
 	int i = 0, j = 0;
 
-	while (*dest)
-		dest++;
-	
 	while (j++ < n)
 		*dest++ = *src++;
 	src = aux2;
