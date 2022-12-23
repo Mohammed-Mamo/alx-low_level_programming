336cad4934c771024c32ead5820b694628a0aa49
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-23 06:37:32 -0800

    stringcopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,6 @@
  * @n: length
  * Return: charca
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
@@ -17,16 +16,13 @@ char *_strncpy(char *dest, char *src, int n)
 		src++;
 		j++;
 	}
-
 	j++;
 
 	if (n > j)
 		n = j;
-
 	src = aux2;
-
 	for (; i < n; i++)
 		*dest++ = *src++;
-
-	return (aux2);
+	dest = aux2;
+	return (dest);
 }
