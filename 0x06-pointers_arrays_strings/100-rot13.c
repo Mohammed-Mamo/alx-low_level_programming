9614bdf7897d51055baba84451cb25a3148f95c4
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-23 09:57:39 -0800

    advanced 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes rotated character
+ * @r: input character
+ * Return: character
+ */
+
+char *rot13(char *r)
+{
+	int i = 0;
+
+	while (r[i] != '\0')
+	{
+		while ((r[i] >= 'a' && r[i] <= 'z') || (r[i] >= 'A' && r[i] <= 'Z'))
+		{
+			if ((r[i] >= 'a' && r[i] <= 'm') || (r[i] >= 'A' && r[i] <= 'M'))
+				r[i] += 13;
+			else
+				r[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (r);
+}
