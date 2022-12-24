666d7e489494349eb3661acfce6b700e50da55e7
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-23 23:48:38 -0800

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *x)
 	int i = 0, j;
 	char NUM[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	char str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	
+
 	while (x[i])
 	{
 		for (j = 0; j < 10; j++)
