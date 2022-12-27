704b05a8cdfdf0748e947944f63f494ad66c1056
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-27 05:16:34 -0800

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,58 +1,25 @@
 #include "main.h"
-int countdigit(int num);
-
 /**
  * print_number - function
- * @n: integer
+ * @n: number tobe tesprinted
  * Return: void
  *
  */
+
 void print_number(int n)
 {
-	int x;
-	int j = n;
+	int i, j;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		j *= -1;
-		x = j;
+		j = -n;
 	}
 
-	if ( j == 0)
-		_putchar('0');
-
-	else if (j <= 9)
-		_putchar(j);
-
-	else
+	while (j / 10 != 0)
 	{
-		x = countdigit(j);
-
-		while ( j / x >= 1)
-		{
-			_putchar((j / x) + '0');
-			j %= x;
-			x /= 10;
-		}
+		_putchar((j / 10));
+		j /= 10;
 	}
-}
-/**
- * countdigit - counts number of digit
- * @num: number to be tested
- * Return: return no of digit
- *
- */
-
-int countdigit(int num)
-{
-	int y = 10;
-	int z = num;
-
-	while (z /10 >= 1)
-	{
-		z /= 10;
-		y *=10;
-	}
-	return (y);
+	_putchar((j % 10) + '0');
 }
