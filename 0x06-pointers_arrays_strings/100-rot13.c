aded75e296aab818a77d9e84a113a93e7217990f
Mohammed-Mamo <m7468.mamo@gmail.com>
2022-12-27 03:07:20 -0800

    mmm

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,20 +10,20 @@ char *rot13(char *s)
 {
 	int i = 0;
 	int j;
-	char array1[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char array1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char array2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-while (s[i] != '\0')
-{
-for (j = 0; j < 52; j++)
-{
-if (s[i] = array1[j])
-{
-s[i] = array2[j];
-break;
-}
-i++;
-}
-}
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == array1[j])
+			{
+				s[i] = array2[j];
+				break;
+			}
+		}
+		i++;
+	}
 return (s);
 }
