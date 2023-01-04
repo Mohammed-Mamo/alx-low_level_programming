f5eb3466dce0029a05714ac296977ffafe0b048f
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-04 13:36:20 -0800

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,8 @@
 #include "main.h"
-#include <stdio.h>/**
-* _strpbrk -  a function that searches a string for any of a set of bytes.
+#include <stdio.h>
+
+/**
+* _strpbrk - a function that searches a string for any of a set of bytes.
 * @s: an input string
 * @accept: an input character with to locate into string s
 * Return:  a pointer to the byte in s that matches one of the bytes in accept,
@@ -10,7 +12,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	char *start = accept;
-	
+
 	while (*s)
 	{
 		while (*accept)
