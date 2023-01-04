d1afd32d7873300ba0e0de80b99c3d991156ba9c
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-04 13:56:57 -0800

    final at 50%

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets value of a pointer
+ * @s: double pointer to a string
+ * @to: pointer to the string to set s to
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
