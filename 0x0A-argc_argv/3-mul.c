952a718a54e03299592c1875b568a43e820fe34b
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-07 15:48:29 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, product;
 
-	if (argc != 3)
+	if ((argc != 3))
 	{
 		printf("%sError\n");
 		return (1);
 	}
 	else
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
+		num1 = argv[1];
+		num2 = argv[2];
 		product = num1 * num2;
 		printf("%d\n", product);
 		return (0);
