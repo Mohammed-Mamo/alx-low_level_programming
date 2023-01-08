5dc6f3640512710fdf7bb113e5b38d9779dc603d
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-08 09:33:27 -0800

    last Updated

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - is a function
+ * @str: is pointer of strings
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
