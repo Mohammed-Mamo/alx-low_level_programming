9d12d87fe61b9828a56d6ce7c9abfaf607f60bc7
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-09 16:13:49 -0800

    last3rd

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		if (nwptr[i] == NULL)
 			return (NULL);
 	}
-	for (i = 0; i < height i++)
+	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 		{
