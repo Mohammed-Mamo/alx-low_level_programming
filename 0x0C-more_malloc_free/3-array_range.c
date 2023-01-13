80dd9ab4cef51e845276e3c603c7a50e7b15b786
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-13 10:46:46 -0800

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,12 +13,12 @@ int *array_range(int min, int max)
 	int i;
 	int *iptr;
 
-	if (max <= min)
+	if (min > max)
 		return (NULL);
-	iptr = malloc((max - min + 1));
+	iptr = malloc(sizeof(int) * (max - min + 1));
 	if (iptr == NULL)
 		return (NULL);
-	for (i = 0; i < (max - min); i++)
+	for (i = 0; i <= (max - min); i++)
 		*(iptr + i) = min + i;
 	return (iptr);
 }
