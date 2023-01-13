606c49b7793ba98d7f73a78f11763721f5c2a1a7
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-13 10:02:06 -0800

    task11

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,31 +6,30 @@
  * @s1: string1
  * @s2: string2
  * @n: size of s2
- * Return: pointer on succes , NULL otherwise
+ * Return: pointer on succes
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int len1 = 0, len2 = 0, len3;
+	int len1 = 0;
+	int len2 = 0;
 	int i = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	while (s1[len1])
 		len1++;
-
 	if (s2 == NULL)
 		s2 = "";
 	while (s2[len2])
 		len2++;
-
-	if (len2 <= n)
+	if (n >= len2)
 		n = len2;
-	len3 = len1 + n + 1;
-	ptr = malloc(len3);
+	ptr = malloc(len1 + n + 1);
 	if (ptr == NULL)
 		return (NULL);
-	for (;i < len3; i++)
+	for (; i < (len2 + n); i++)
 	{
 		if (i < len1)
 		{
@@ -40,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		else
 		{
 			ptr[i] = *s2;
-				s2++;
+			s2++;
 		}
 	}
 	ptr[i] = '\0';
