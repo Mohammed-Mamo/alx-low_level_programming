f2a62ac72f772933ee8138a1d5144d771fd8281e
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-13 21:52:04 -0800

    taskadvanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,6 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		old_size = new_size;
 	for (i = 0; i < old_size; i++)
 		nlocation[i] = nptr[i];
+
 	free (ptr);
 	return (nlocation);
 }
