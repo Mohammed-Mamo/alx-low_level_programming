1c90ebe073744e5a73162d064958a4e85e330e8e
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-16 07:29:46 -0800

    typedefined

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,12 @@
-#include <stdlib.h>
 /**
  * struct dog - is object dog
  * @name: name of the dog
  * @owner: owner name
  * @age: age of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+}dog_t;
