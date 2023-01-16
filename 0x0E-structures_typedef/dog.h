32bfc764e6039693afde2263149724127c7423a6
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-16 07:58:56 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,11 @@
  * @owner: owner name
  * @age: age of the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
