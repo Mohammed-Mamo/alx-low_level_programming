da899b720921ae79b3161dc84935a3cf3c3c317f
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-16 12:43:16 -0800

    lastlast

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,34 +12,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
 	char *cpyn, *cpyo;
 	int len1 = 0, len2 = 0, i;
+
 	len1 = strlen(name);
 	len2 = strlen(owner);
 
-	cpyn = malloc(len1 + 1);
+	if (name == NULL || owner == NULL)
+	return (NULL);
 
+	cpyn = malloc(len1 + 1);
 	if (cpyn == NULL)
 		return (NULL);
-
 	cpyo = malloc(len2 + 1);
 	if (cpyo == NULL)
 		return (NULL);
-
 	dog_t *newd;
-
-	newd->name = malloc(len1 + 1);
-	newd->owner = malloc(len2 + 1);
-	newd->age = malloc(sizeof(float));
-
+	newd = malloc(len1 + len2 + 1);
 	if (newd == NULL)
-		return (NULL);
+	return (NULL);
 
-	for (i = 0; name[i]; i++)
+	for (i = 0; i < len1; i++)
 		cpyn[i] = name[i];
 	cpyn[i] = '\0';
-	for (i = 0; owner[i]; i++)
+
+	for (i = 0; i < len2; i++)
 		cpyo[i] = owner[i];
 	cpyo[i] = '\0';
 	newd->age = age;
