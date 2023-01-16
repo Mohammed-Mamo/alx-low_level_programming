35f2ef8757bf7cdae684e3c9d722bfad8e35c2f3
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-16 07:56:55 -0800

    DO_H updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _DOG_H
+#define _DOG_H
 /**
  * struct dog - is object dog
  * @name: name of the dog
@@ -9,4 +11,10 @@ typedef struct dog
 	char *name;
 	char *owner;
 	float age;
-}dog_t;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
