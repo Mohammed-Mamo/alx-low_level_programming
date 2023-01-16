e514c1baf9d3e7b1f975a4d9dfdfab0520b4d733
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-16 12:27:14 -0800

    last

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,42 +1,49 @@
-#include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
-/**
- * dog_t new_dog - is function
- * @name: name of the dog
- * @age: age of the new dog
- * @owner: the dog owner
- * Return: a pointer
-*/
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
 
+/**
+ * new_dog - is function
+ * @name: Dogs name
+ * @age: Dogs age
+ * @owner: owners of dog
+ * Return: pointer
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || owner == NULL)
+
+	char *cpyn, *cpyo;
+	int len1 = 0, len2 = 0, i;
+	len1 = strlen(name);
+	len2 = strlen(owner);
+
+	cpyn = malloc(len1 + 1);
+
+	if (cpyn == NULL)
 		return (NULL);
 
-	dog_t *nd;
+	cpyo = malloc(len2 + 1);
+	if (cpyo == NULL)
+		return (NULL);
 
-	nd = malloc(sizeof(dog_t));
-	if (nd == NULL)
+	dog_t *newd;
+
+	newd->name = malloc(len1 + 1);
+	newd->owner = malloc(len2 + 1);
+	newd->age = malloc(sizeof(float));
+
+	if (newd == NULL)
 		return (NULL);
-	char *cpyName, *cpyOwner;
-	int l1 = 0, l2 = 0, i = 0;
-	while (name[l1])
-		l1++;
-	while (owner[l2])
-		l2++;
-	cpyName = malloc(l1 + 1);
-	cpyOwner = malloc(l2 + 1);
-	if (cpyName == NULL || cpyOwner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyOwner[i] = owner[i];
-	cpyOwner[i] ='\0';
+
 	for (i = 0; name[i]; i++)
-		cpyName[i] = name[i];
-	cpyName[i] = '\0';
-	nd->name = cpyName;
-	nd->age = age;
-	nd->owner = cpyOwner;
-	return (nd);
+		cpyn[i] = name[i];
+	cpyn[i] = '\0';
+	for (i = 0; owner[i]; i++)
+		cpyo[i] = owner[i];
+	cpyo[i] = '\0';
+	newd->age = age;
+	newd->owner = owner;
+	newd->name = name;
+	return (newd);
 }
