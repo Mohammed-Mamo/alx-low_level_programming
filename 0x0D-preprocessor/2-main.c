df45c0d12e9f10a83c1d97fc018d76413c9c6204
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-17 06:05:25 -0800

    printfilename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
+ * main - prints the name of file
  *
- *  main - prints the name of file
- *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
