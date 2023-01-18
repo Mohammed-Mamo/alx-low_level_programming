f6967cea771a317fb33e7a9a4fe64e316ec52ce7
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-18 11:44:53 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * print_name - is function to display the name
+ * @name: name to be printed
+ * @f: function name
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
