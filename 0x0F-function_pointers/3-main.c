b69950e847c3b53f6832faeba9c678d8fe549c3b
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-25 23:24:50 -0800

    tas review

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,16 +12,17 @@
 int main(int argc, char *argv[])
 {
 	int (*func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("ERROR\n");
-		exit (98);
+		exit(98);
 	}
 	func = get_op_func(argv[2]);
 	if (func == NULL)
 	{
 		printf("ERROR\n");
-		exit (99);
+		exit(99);
 	}
 	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
