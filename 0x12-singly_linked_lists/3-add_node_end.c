6001fb3652ebc0dea0a994df2ab08731724fd1fd
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-27 13:07:17 -0800

    revised

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,11 +14,14 @@ list_t *add_node(list_t **head, const char *str)
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
+
 	while (str[length])
+	{
 		length++;
-	temp->len = length;
-	temp->str = strdup(str);
-	temp->next = *head;
-	*head = temp;
+		temp->len = length;
+		temp->str = strdup(str);
+		temp->next = *head;
+		*head = temp;
+	}
 	return (temp);
 }
