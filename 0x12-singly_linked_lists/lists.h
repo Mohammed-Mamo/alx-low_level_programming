92f474957cd4e1fa8b237137f4c3186e23870947
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-27 12:31:04 -0800

    task0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,16 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stdlib.h>
+/**
+ * @list_s: datata type struct
+ *
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+}list_t;
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
