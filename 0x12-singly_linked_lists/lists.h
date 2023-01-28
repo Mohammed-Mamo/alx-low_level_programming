9dad32e5d10671e856649521ec36338607e2755f
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-28 07:06:27 -0800

    lists.h updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,15 +2,18 @@
 #define LISTS_H
 #include <stdlib.h>
 /**
- * @list_s: datata type struct
- *
+ * struct list_s - singly linked list
+ * @str: string to be allocated using malloc function
+ * @len: length of the string
+ * @next: pointer that points to the nxt node
+ * Description: Data structure uses
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
