8ebb561a18b12671511f796773b495e7077871c9
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-01-28 09:06:44 -0800

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,18 @@
 extern printf
-section .data
-msg: db "Hello, Holberton", 0
-fmt: db "%s", 10,0
-section .text
-global main
+
+	section .data
+msg:		db "Hello, Holberton", 0
+fmt:		db "%s", 10, 0
+
+	section .text
+	
+	global main
 main:
-push rbp
-mov rdi,fmt
-mov rsi,msg
-mov rax,0
-call printf
-pop rbp
-mov rax rax,0
-ret
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax rax,0
+	ret
