62cbe6216657bd6cf9d43c38fd3289d1324003fb
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-02-01 17:19:53 -0800

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,18 +7,19 @@
  * @n: An integer input for new node
  * Return: The address to new node at specified index, or NULL if it failed
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int j = 1;
 	listint_t *new_nd, *tmp;
-
+	
 	if (head == NULL)
 		return (NULL);
-
+	
 	new_nd = malloc(sizeof(listint_t));
 	if (new_nd == NULL)
 		return (NULL);
-
+	
 	if (*head == NULL)
 	{
 		*head = new_nd;
@@ -26,7 +27,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_nd->n = n;
 		return (new_nd);
 	}
-
 	if (idx == 0)
 	{
 		new_nd->next = *head;
