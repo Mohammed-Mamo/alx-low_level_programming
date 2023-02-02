3bb2198625be0462524c663f8423a18f38407629
Mohammed-Mamo <m7468.mamo@gmail.com>
2023-02-02 11:42:37 -0800

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,6 +5,7 @@
 /**
 * print_binary - A function that prints a binary number without % or /
 * @n: The number to be printed
+*
 * Return: Nothing
 */
 
@@ -17,11 +18,11 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 		return;
 	}
-	
 	while (max)
 	{
 		if (flag == 1 && (n & max) == 0)
 			_putchar('0');
+
 		else if ((n & max) != 0)
 		{
 			_putchar('1');
