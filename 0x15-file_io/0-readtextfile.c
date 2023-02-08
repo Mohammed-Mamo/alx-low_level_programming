65e11fce0902c3474e8a6e5cc4888cb8bb6d49da
Mohammed-Mamo <117733585+Mohammed-Mamo@users.noreply.github.com>
2023-02-08 22:54:47 +0300

    Update 0-readtextfile.c

--- 0x15-file_io/0-readtextfile.c
+++ 0x15-file_io/0-readtextfile.c
@@ -1,41 +1,36 @@
 #include "main.h"
+
 /**
- * read_textfile - this function reads a text file and prints it to the POSIX
- * @filename: name of the file
- * @letters: number of letters
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: the number of letters
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int FN, r, w;
-	char *buffer = malloc(sizeof(char) * letters);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
 
-/*OPEN*/
-	FN = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (FN == -1)
-	{
+	if (fd == -1)
 		return (0);
-	}
-/*READ*/
-	r = read(F, buffer, letters);
-	if (r == -1)
-	{
-		return (0);
-	}
-/*WRITE*/
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1)
-	{
-		return (0);
-	}
-	close(FN);
 
-	return (w);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
