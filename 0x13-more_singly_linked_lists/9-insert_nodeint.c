560caaa9462944885e69f9de8ab783f17adb5857
Mohammed-Mamo <117733585+Mohammed-Mamo@users.noreply.github.com>
2023-02-10 02:38:54 +0300

    Update 9th task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,40 +10,36 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int j = 1;
-	listint_t *new_nd, *tmp;
-	
-	if (head == NULL)
-		return (NULL);
-	
-	new_nd = malloc(sizeof(listint_t));
-	if (new_nd == NULL)
-		return (NULL);
-	
-	if (*head == NULL)
-	{
-		*head = new_nd;
-		new_nd->next = NULL;
-		new_nd->n = n;
-		return (new_nd);
-	}
-	if (idx == 0)
-	{
-		new_nd->next = *head;
-		new_nd->n = n;
-		*head = new_nd;
-		return (new_nd);
-	}
+unsigned int j = 1;
+listint_t *new_nd, *tmp;
 
-	tmp = *head;
-	while (j < idx)
-	{
-		tmp = tmp->next;
-		j++;
-	}
-
-	new_nd->n = n;
-	new_nd->next = tmp->next;
-	tmp->next = new_nd;
-	return (new_nd);
+if (head == NULL)
+return (NULL);
+new_nd = malloc(sizeof(listint_t));
+if (new_nd == NULL)
+return (NULL);
+if (*head == NULL)
+{
+*head = new_nd;
+new_nd->next = NULL;
+new_nd->n = n;
+return (new_nd);
+}
+if (idx == 0)
+{
+new_nd->next = *head;
+new_nd->n = n;
+*head = new_nd;
+return (new_nd);
+}
+tmp = *head;
+while (j <= idx)
+{
+tmp = tmp->next;
+j++;
+}
+new_nd->n = n;
+new_nd->next = tmp->next;
+tmp->next = new_nd;
+return (new_nd);
 }
