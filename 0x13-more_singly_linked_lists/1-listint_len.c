faf191c7e888715129af4f476a6ac1caafca5784
Mohammed-Mamo <117733585+Mohammed-Mamo@users.noreply.github.com>
2023-02-10 01:52:40 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,12 @@
 */
 size_t listint_len(const listint_t *h)
 {
-  size_t nodes = 0;
-  
-  while (h)
-  {
-	  h = h->next;
-	  nodes++;
-  }
-  return (nodes);
+size_t nodes = 0;
+
+while (h)
+{
+h = h->next;
+nodes++;
+}
+return (nodes);
 }
